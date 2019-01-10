fb1b23cf2b6f0c0caf63fa3690c15810f5386417
M.S. Patterson <tertiarymatt@gmail.com>
2019-01-10 16:14:54 -0700

    updated script to indicate which GEE scripts to use when, and also improved documentation

--- 01_samplesize.R
+++ 01_samplesize.R
@@ -11,7 +11,11 @@
 library(tidyverse)
 source("00_functions.R")
 
-#' ### This section is used to determing sample size needed for the project.
+#' ### This section is used to determing sample size needed for the project.  
+#' Before running this script, use the `01_ChangePercentage` Earth Engine script
+#' to find the percent area occupied by change classes in the output data. That 
+#' percentage will be used in the sample size allocation optimizing algorithm
+#' included in this script. 
 #' 
 #+ Sample Size
 
@@ -32,9 +36,14 @@ actualN <- 5500
 #' Use the optimization tool from Wagner & Stehman, 2015 to split samples into
 #' two pools, stable and change.
 
-#' The first step is to create an area-based error matrix, with estimated proportions. 
+#' The first step is to create an area-based error matrix, with estimated 
+#' proportions. Position [1,1] in this matrix is the estimate of change area,
+#' position [2,2] in the matrix is stable class area. Positions [1,2] and [2,1]
+#' represent the errors between the change and stable classes. These are 
+#' obviously unknown, but can be estimated. Here they are conservatively 
+#' estimated as being one quarter of the size of the change class. 
 
-#+ Create Error Matrix.
+#+ Create Error Matrix. 
 
 #'We will assume a larger than realistic change to ensure sufficient power. 
 error <- matrix(nrow = 2, ncol = 2, 
@@ -49,48 +58,7 @@ checkErrorMatrix(error)
 samplePool <- optimizeSplit(error, actualN, c("Change", "Stable"))
 samplePool
 
-#' The next step requires the use of Google Earth Engine to assign the split
-#' samples to their respective classes, and generating a point file. R could be
-#' used for this, but this would require that the map be downloaded and imported. 
-#' The Earth Engine script is included below. 
-#' 
-#' > //Calculate area of each class, turn into proportion
-#' > 
-#' > // Stable classes get 4244 points.  
-#' > // Changes classes get 1256 points. 
-#' > // Minimum of 75 points per class. 
-#' > var stable = [];
-#' > var change = [];
-#' >
-#' > // Stack class map and latitude/longitude  
-#' > var sampleImage = ee.Image.cat(toSample, ee.Image.pixelLonLat());  
-#' >   
-#' > // Sample within the study area and the loss mask     
-#' > var sample = sampleImage.stratifiedSample({    
-#' >  classValues: [],  
-#' >	classPoints: [stable, change],  
-#' >  classBand: 'class',   
-#' >  region: studyArea,    
-#' >  scale: 30,    
-#' >  seed: 17,     
-#' > });  
-#' >     
-#' >   // Add back in geometry and visualize    
-#' > var sample_geo_assembly = sample.map(function(point){    
-#' >   var long = point.get('longitude');    
-#' >   var lat = point.get('latitude');    
-#' >   var geom = ee.Algorithms.GeometryConstructors.Point([long, lat]);    
-#' >   return point.setGeometry(geom);    
-#' > });    
-#' >     
-#' > Map.addLayer(sample_geo_assembly.draw('0000FF',2),{},'Sample locations');   
-#' > Map.centerObject(sample_geo_assembly, 9);  
-#' >   
-#' > print('Point Sample Data', sample_geo_assembly);  
-#' >   
-#' > Export.table.toDrive({  
-#' >   collection: sample_geo_assembly,  
-#' >   description:'Validation_Sample',  
-#' >   folder:'driveFolderHere',  
-#' >   fileFormat: "CSV"  
-#' >   });  
+#' The next step requires the use of the Google Earth Engine script 
+#' `02_SamplePointGenerator` to assign the split samples to their respective 
+#' classes, and generating a point file. R could be used for this, but this 
+#' would require that the map be downloaded and imported. 
