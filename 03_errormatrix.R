bc54aea7f2728015acaab3ec359aacbf2943981c
M.S. Patterson <tertiarymatt@gmail.com>
2019-01-11 20:37:35 -0700

    begin error matrix file

---
+++ 03_errormatrix.R
@@ -0,0 +1,7 @@
+#' ---
+#' title: "Error Matrix Generation"
+#' author: "MS Patterson, tertiarymatt@gmail.com"
+#' date: "`r format(Sys.time(), '%B %d, %Y')`"
+#' output: github_document
+#' ---
+#'
