f76532ba19430fef3d5e00b0ce62f0323d797db9
M.S. Patterson <tertiarymatt@gmail.com>
2019-01-11 21:26:22 -0700

    outlining workflow

--- 03_errormatrix.R
+++ 03_errormatrix.R
@@ -5,3 +5,19 @@
 #' output: github_document
 #' ---
 #'
+
+#' ### Required packages
+#+ Packages
+library(tidyverse)
+library(irr)
+library(e1701)
+source("00_functions.R")
+
+#' Task list for this script:
+#' 1. Bring in validation data. Need to have map values attached.
+#' 1. Convert map values and validation values to factors.
+#' 1. Ensure factors have identical levels.
+#' 1. Construct confusion matrix.
+#' 1. Convert to area-based. 
+#' 1. Calculate standard errors of area estimates. 
+#' 1. Calculate error metrics.
