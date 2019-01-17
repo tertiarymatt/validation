c10dd0ead4be52422e11a4fccc1378d37ed41c2f
M.S. Patterson <tertiarymatt@gmail.com>
2019-01-17 11:44:34 -0700

    moved material from the error matrix file into scratch, removed scripts

--- 99_scratch.R
+++ 99_scratch.R
@@ -17,16 +17,61 @@
 #' Note that columns of responses need to be set up as factors. 
 
 #'+ make a confusionMatrix
+#' Task list for this script:  
+#' 1. DONE. Bring in validation data. Need to have map values attached.  
+#' 1. DONE. Convert map values and validation values to factors.  
+#' 1. DONE. Ensure factors have identical levels.  
+#' 1. DONE. Construct confusion matrix.  
+#' 1. Convert to area-based error matrix.   
+#' 1. Calculate standard errors of area estimates. 
+#' 1. Calculate error metrics.  
+#' 
 
-#' Then need to conver the confusion matrix into an area-based version. 
-#' Will need pixel counts and so forth for this. 
+#' ### Data Prep  
+#' Import the validation data. Either properly configure and source
+#'  02_dataprep.R or run it manually.
 
-#'+ make confusionMatrix area-based
+#+ Import
+# Import the collected validaiton data and map values. 
+source("02_dataprep.R")
 
-#' Using new area-based confustion matrix, calculate accuracy statistics, and
-#' standard errors.
+#' The error matrix tool works with factors. The data have been imported and
+#' prepared as characters. Convert map values and validation values to factors.
 
-#'+ tidy confusionMatrix, extract statistics
+#+ ConvertoClassesandFactors
+#Code to convert these classes into factors, for use in building an error matrix.
+
+# convert integers into text
+finalTable <- convertToClasses(reclassed)
+
+#strip out No_Data entries.
+toRemove <- which(finalTable$LEVEL2 == "No_Data")
+finalTable <- finalTable[-toRemove,]
+
+# Convert to factors.
+
+refLevels <- c("Primary_Forest", "Secondary_Forest", "Plantation_Forest", 
+							 "Mangrove", "Cropland", "Natural_Water",  "Artificial_Water",
+							 "Paramo", "Shrubland", "Herbland", "Settlement", "Infrastructure", 
+							 "Glacier", "Non-vegetated")
+
+reference <- factor(finalTable$LEVEL2, refLevels)
+map <- factor(finalTable$MapClass, refLevels)
+
+#' ###A Basic Confusion Matrix 
+#' Construct the confusion matrix, using the extracted vectors of values.
+#' We can use the `caret` package for this process. 
+
+#+ ConfusionMatrix
+errorMatrix <- confusionMatrix(map, reference, positive = NULL, 
+															 dnn = c("Prediction", "Reference"))
+
+#' ### Create an area-based data frame.  
+
+#+ AreaBased
+# Extract the error matrix, and make into a data frame for easier addressing.
+# Remember that reference are columns, and predicted map values are rows!
+eM <- data.frame(unclass(errorMatrix$table))
 
 
 
