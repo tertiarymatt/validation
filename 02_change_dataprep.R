6c0951dcd36730e7710964cc4613d2e9d098223b
M.S. Patterson <tertiarymatt@gmail.com>
2019-01-18 12:41:27 -0700

    added material to produce a csv of areas

--- 02_change_dataprep.R
+++ 02_change_dataprep.R
@@ -25,6 +25,7 @@ source('00_functions.R')
 #' 1. Convert dominant point classes to LULC classes. 
 #' 1. Format for use in SEPAL.  
 #' 1. Export the data. 
+#' 1. Export class areas for use with the SAE-Analysis tool.  
 #' 
 #' ### Importing data.  
 #' Import raw data, strip out unneeded name components of class fields. 
@@ -192,3 +193,36 @@ finalTable$predicted <- as.numeric(finalTable$predicted) - 1
 
 # Export table for upload to SEPAL
 write_csv(finalTable, "data/finalTable.csv")
+
+#' ### Importing class area data.  
+#' Import class data, reformat the feature properties to make a tidy export.
+#' Earth Engine exports a set of lists, with numbers formatted in a slightly
+#' odd manner. The lines below convert this into a clean set of areas and 
+#' associated classes. 
+
+#+ AreaData
+rawAreas <- read_csv("data/Class_Areas.csv")
+
+#clean up GEE formatting
+areas <- gsub("[", "", rawAreas$area, fixed = TRUE) %>% 
+	gsub("]", "", ., fixed = TRUE) %>% 
+	strsplit(., ", ")
+
+#take number strings and split into the base number and an exponent
+areas <- areas[[1]]
+areaTable <- str_split(areas, "E", simplify = T)
+number <- as.numeric(areaTable[,1])
+exponent <- as.numeric(areaTable[,2])
+
+#replace missing exponents with 0, so those numbers are multiplied by 1. 
+exponent[is.na(exponent)] <- 0
+areas <- number * 10 ^ exponent
+
+areaClasses <- gsub("[", "", rawAreas$classes, fixed = TRUE) %>% 
+	gsub("]", "", ., fixed = TRUE) %>% 
+	strsplit(., ", ")
+
+cleanAreas <- data.frame(areas, areaClasses[[1]])
+
+# Export table for upload to SEPAL
+write_csv(cleanAreas, "data/area_rast.csv")
