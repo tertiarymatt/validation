b8c21db78347a2388a6b57427ef8cf9deb062037
M.S. Patterson <tertiarymatt@gmail.com>
2019-01-22 13:53:35 -0700

    updating QA/QC

--- 98_QAQC.R
+++ 98_QAQC.R
@@ -14,148 +14,129 @@ setwd("~/R/projects/validation")
 #+ Packages
 library(tidyverse)
 library(irr)
-library(stargazer)
+library(knitr)
 source("00_functions.R")
 
 #' ### Import Data
+#' Each worker or team should have the data they produced for the training data
+#' sets imported. There should be no need to process the data exported from CEO
+#' prior to working with it in this script. 
+
 #+ Import
-
-
+# add a line for each worker or team. 
+worker1 <- read_csv("data/ceo-training-project-3.1-plot-data-2019-01-07.csv")
+worker2 <- read_csv("data/ceo-training-project-3.2-plot-data-2019-01-07.csv")
 
 #' ### Process data into form that can be used for irr  
-#' Provide overview of the process. 
+#' The imported data needs to be assembled and then cleaned up to make it easier
+#' to read and work with. 
 
 #+ Process
+# make into one data frame. 
+crossData <- bind_rows(worker1, worker2)
 
+# Find and extract the class names. 
+names(crossData)
+
+# create class column object to use in script.
+# !!!If the structure of the data changes this MUST be updated!!!
+classCol <- c(18:36)
+
+classes <- colnames(crossData[classCol]) %>% 
+	gsub(":", "_", .) %>% 
+	gsub(" ", "_", .) %>% 
+	gsub("/", "_", .)
+
+# rename the columns in the data with the cleaned up version
+colnames(crossData)[classCol] <- classes
+names(crossData)
+
+# Now that the data is cleaned up, it needs to be processed into a form that 
+# can be used for by the `irr` package. To do this, need to prepare a list of 
+# single class matrices with the PI values produced by each worker separated 
+# into columns. 
+
+#+ PrepforIRR
+
+# create an empty list to populate
+cross_tables <- rep(list(NA),length(classes))
+names(cross_tables) <- classes
+
+# generate the matrices
+for (m in 1:length(cross_tables)) {
+	cross_tables[[m]] <- select(crossData, "USER_ID", "PLOT_ID", classes[m]) %>%
+		spread(., "USER_ID", classes[m]) %>%
+		.[,-1] %>%
+		na.omit(.) %>% 
+		as.matrix(.)
+}
 
 
 #' ### Calculate Metrics of Agreement  
-#' Describe metrics used, provide citations. 
-
-#+ IRR Metrics
-
-
-
-
-#' ### Material below this line is imported from another project, as examples.
+#' **Iota** is used to calculate overall agreement between raters, and represents
+#' an overall look at how close they agree. A more granular approach is necessary
+#' to improve agreement, but iota provides a useful summary.  
 #' 
-#+  Old Code
-# Import Data
-# cross1 <- read_csv("data/ceo-assembly-cross-validation-5groups-result.csv")
-# 
-# # Process data into form that can be used for irr
-# classes <- colnames(cross1)[11:30]
-# 
-# cross_tables <- rep(list(NA),length(classes))
-# 
-# names(cross_tables) <- classes
-# 
-# for(m in 1:length(cross_tables)) { 
-#   cross_tables[[m]] <- select(cross1, "Group", "PLOT_ID", classes[m]) %>% 
-#     spread(., "Group", classes[m]) %>% 
-#     .[,-1] %>%
-#     as.matrix(.)
-# }
-# 
-# # Calculate metrics of agreement ----
-# 
-# # Iota, a multivariate metric of agreement
-# crossval_iota <- iota(cross_tables, scaledata = "q")
-# 
-# # Intraclass Correlation Coefficient
-# cross_icc <- list()
-# for(m in 1:length(cross_tables)) { 
-#   cross_icc[[m]] <- icc(cross_tables[[m]], model="oneway", type="agreement")
-# }
-# names(cross_icc) <- classes
-# 
-# 
-# # make a "table" 
-# icc_values <- matrix(NA, 20, 2)
-# for(m in 1:length(cross_icc)) { 
-#   icc_values[m,1] <- names(cross_icc[m])
-#   icc_values[m,2] <- cross_icc[[m]]$value
-# }
-# 
-# 
-# # Krippendorf's alpha
-# cross_ka <- list()
-# for(m in 1:length(cross_tables)) { 
-#   cross_ka[[m]] <- kripp.alpha(t(cross_tables[[m]]), method="ratio")
-# }
-# names(cross_ka) <- classes
-# 
-# #Mean Bivariate Pearson's
-# cross_cor <- list()
-# for(m in 1:length(cross_tables)) { 
-#   cross_cor[[m]] <- meancor(cross_tables[[m]])
-# }
-# names(cross_cor) <- classes
-# 
-# 
-# # making pretty tables for output to paper ----
-# icc_comp[,2:4] <- round(icc_comp[,2:4], 4)
-# 
-# stargazer(icc_comp, 
-#           title = "Changes in intraclass correlation coefficient (ICC) between training sessions. 
-#           Dashes indicate classes with too few non-zero values to calculate.", summary = F, 
-#           colnames = T, column.labels = c("Class Names","ICC Session 1", "ICC Session 2", "Change"), rownames = F)
-# 
-# # Concordance on Reclassed Data ----
-# 
-# newCross01 <- cross1 %>% 
-#   transmute(., 
-#             PlotID = PLOT_ID,
-#             Group = Group,
-#             Flagged = FLAGGED,
-#             Forest = FOREST_TREE, 
-#             Mangrove = MANGROVE,
-#             Grass = GRASS,
-#             Shrub = SHRUB_W_WOOD,
-#             Crop = CROP_PLANTATION_ORCHARD + CROP,
-#             Rice = PADDY_RICE,
-#             Impervious = IMPERVIOUS_BUILT_SURFACE,
-#             BuiltVeg = BUILT_TREE + BUILT_VEGETATION,
-#             Wetlands = WETLANDS_MUDFLAT_TIDALFLAT + AQUATIC_VEGETATION_FLOODED_FOREST,
-#             Water = WATER + AQUACULTURE_POND,
-#             Snow = SNOW_ICE,
-#             Barren = BARREN_SAND_DUNE + ROCKY_MOUNTAIN + MINING,
-#             Other = OTHER + UNKNOWN)
-# 
-# 
-# # Process data into form that can be used for irr
-# classes02 <- colnames(newCross01)[04:16]
-# 
-# cross01_tables <- rep(list(NA),length(classes02))
-# 
-# names(cross01_tables) <- classes02
-# 
-# 
-# for(m in 1:length(cross01_tables)) { 
-#   cross01_tables[[m]] <- select(newCross01, "Group", "PlotID", classes02[m]) %>% 
-#     spread(., "Group", classes02[m]) %>% 
-#     .[,-1] %>%
-#     as.matrix(.)
-# }
-# 
-# 
-# # Calculate metrics of agreement for second classification scheme----
-# 
-# # Iota, a multivariate metric of agreement
-# newCross01_iota <- iota(cross01_tables, scaledata = "q")
-# 
-# # Intraclass Correlation Coefficient
-# newCross01_icc <- list()
-# for(m in 1:length(cross01_tables)) { 
-#   newCross01_icc[[m]] <- icc(cross01_tables[[m]], model="oneway", type="agreement")
-# }
-# names(newCross01_icc) <- classes02
-# 
-# 
-# # make a "table" 
-# newIcc_values <- matrix(NA, 13, 2)
-# for(m in 1:length(newCross01_icc)) { 
-#   newIcc_values[m,1] <- names(newCross01_icc[m])
-#   newIcc_values[m,2] <- newCross01_icc[[m]]$value
-# }
+#' #### Citations  
+#' 1. Conger, A.J. (1980). Integration and generalisation of Kappas for multiple
+#'  raters. Psychological Bulletin, 88, 322-328.  
+#' 1. Janson, H., & Olsson, U. (2001). A measure of agreement for interval or
+#'  nominal multivariate observations. Educational and Psychological Measurement, 61, 277-289. 
 
+#+ Iota, a multivariate metric of agreement
+crossval_iota <- iota(cross_tables, scaledata = "q")
+crossval_iota
+
+#' For checking agreement of individual classes, we can use several approaches.  
+#' The **intraclass correlation coefficient** and 
+#' **mean bivariate Pearson's** are two.
+#' The ICC is used to measure consistency between two raters, and uses an F-test
+#' to test for significance.  
+#'  
+#' #### Citations  
+#'  1. Bartko, J.J. (1966). The intraclass correlation coefficient as a measure of 
+#'  reliability. Psychological Reports, 19, 3-11.  
+#'  1. McGraw, K.O., & Wong, S.P. (1996), Forming inferences about some intraclass 
+#'  correlation coefficients. Psychological Methods, 1, 30-46.  
+#'  1. Shrout, P.E., & Fleiss, J.L. (1979), Intraclass correlation: uses in 
+#'  assessing rater reliability. Psychological Bulletin, 86, 420-428.  
+
+#+ Per-class agreement
+
+# Intraclass Correlation Coefficient
+cross_icc <- list()
+for (m in 1:length(cross_tables)) {
+	cross_icc[[m]] <- icc(cross_tables[[m]], model = "oneway", type = "agreement")
+}
+names(cross_icc) <- classes
+
+# make a "table" from data values in list
+icc_values <- data_frame(length(classes), 5)
+for (m in 1:length(cross_icc)) {
+	icc_values[m,1] <- names(cross_icc[m])
+	icc_values[m,2] <- round(cross_icc[[m]]$value, 4)
+	icc_values[m,3] <- round(cross_icc[[m]]$lbound, 4)
+	icc_values[m,4] <- round(cross_icc[[m]]$ubound, 4)
+	icc_values[m,5] <- round(cross_icc[[m]]$p.value, 4)
+}
+
+colnames(icc_values) <- c("Class", "ICC", "Lower", "Upper", "Pvalue")
+
+# Mean Bivariate Pearson's
+cross_cor <- list()
+for (m in 1:length(cross_tables)) {
+	cross_cor[[m]] <- meancor(cross_tables[[m]])
+}
+names(cross_cor) <- classes
+
+# make a "table" from data values in list
+cor_values <- data_frame(length(classes), 3)
+for (m in 1:length(cross_icc)) {
+	cor_values[m,1] <- names(cross_cor[m])
+	cor_values[m,2] <- round(cross_cor[[m]]$value, 4)
+	cor_values[m,3] <- round(cross_cor[[m]]$p.value, 4)
+}
+colnames(cor_values) <- c("Class", "Cor", "Pvalue")
+
+# Assemble tables into one object for display. 
+kable(bind_cols(icc_values, cor_values[,2:3]))
