c38f8b52a00e476e3ed1f9fca619d645ce1c5bf5
M.S. Patterson <tertiarymatt@gmail.com>
2019-03-07 16:31:49 -0700

    updating for final validation run

--- 99.3_scratch_TODO.R
+++ 99.3_scratch_TODO.R
@@ -6,29 +6,29 @@
 #' ---
 #'
 #' ##TODO  
-#' 0. [x] **Collate CEO outputs:** Bring together all samples, unify with original 
+#' 0. [x] **orig_strata:** More or less done when the collation is complete,
+#' can be pulled out of that data.  
+#' 1. [x] **strata_totals:** exported with samples, needs to be processed and
+#'  converted into pixel counts. Trivial.  
+#' 2. [x] **Collate CEO outputs:** Bring together all samples, unify with original 
 #' data derived from GEE/MAE to get stratifying map classes and old validation
 #' back into the file. Should be able to join on Lat/Long. 
-#' 0. [x] **Process CEO outputs:** run through data prep scripts to get ref classes.
+#' 3. [] **map_label:**  Data goes back into GEE to sample final maps.
+#' Obviously constrained by existence of final maps.  
+#' 4. [] **Process CEO outputs:** run through data prep scripts to get ref classes.
 #' Will likely require updating the functions, again. Might have to wait on 
 #' this step for the actual maps to be output, to avoid another reclass on the 
 #' data. Potential chokepoint.  
-#' 1. [x] **orig_strata:** More or less done when the collation is complete,
-#' can be pulled out of that data.  
-#' 2. [x] **ref_label:**  Pull out of processed CEO data. Can be completed prior
+#' 5. [] **ref_label:**  Pull out of processed CEO data. Can be completed prior
 #' to having final maps, but is potential chokepoint linked to processing.    
-#' 3. [x] **map_label:**  Data goes back into GEE to sample final maps.
-#' Obviously constrained by existence of final maps.    
-#' 4. [x] **strata_totals:** exported with samples, needs to be processed and
-#'  converted into pixel counts. Trivial.  
-#' 5. [x] **sample_totals:** Derived from a summary of the collatted CEO data.      
-#' 6. [x] **rfcodes:**  Easy-peasy lemon squeezy. Reclass possible, though.  
-#' 7. [x] **pixel**  = 30.  
-#' 8. [x] **totarea_pix:** Easy to get from total areas.  
-#' 9. [x] **Analyze:** Use new functions to rebalance sample and calculate 
+#' 6. [] **sample_totals:** Derived from a summary of the collatted CEO data.      
+#' 7. [] **rfcodes:**  Easy-peasy lemon squeezy. Reclass possible, though.  
+#' 8. [] **pixel**  = 30.  
+#' 9. [] **totarea_pix:** Easy to get from total areas.  
+#' 10. [] **Analyze:** Use new functions to rebalance sample and calculate 
 #' accuracies. Rejoice, maybe!  
-#' 10. [x] **DREAD:** Reclass to improve accuracy and re-run.  
-#' 11. [x] **Report:** Outline shortly, then simply fill in. 
+#' 11. [] **DREAD:** Reclass to improve accuracy and re-run.  
+#' 12. [] **Report:** Outline shortly, then simply fill in. 
 #' 
 #' Scratch area below:
 #+ scratch
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # validation
 A collection of scripts used for validation/accuracy assessment of land cover products.
+
+## Basic Workflow
+
+### Generate Samples
+To generate samples, a combination of Google Earth Engine (GEE) and R scripts are used. The [first GEE script](https://code.earthengine.google.com/5c56f4448e391a3d79e58a3c94d7fbfe) estimates the area of change in the study area. This is used to inform the first R script (01_samplesize.R) to optimize the split between the stable and change classes. 
+
+After a sample size has been generated, the [second GEE script](https://code.earthengine.google.com/c80b40479080b72ea12a59c13e0cdeea) and [third GEE scripts](https://code.earthengine.google.com/a16be02f16cd1c167d9afff70cc89064) are used to generate the points for the stable and change classes, respectively. These points are exported to two CSV files. Class areas are also exported, which are essential for performing validation when using a map with strata that do not match the original stratification map. 
+
+### PI Work
+The two CSV files are then concatenated, and ordered such that they can be imported into Collect Earth Online (CEO); the field order (and names) must be Longtidue, Latitude, and Plotid. Projects are constructed in CEO and assigned, and Photo Interpretation performed to collect data. 
+
+Once the PI work is complete, the resulting tables are exported from CEO and brought together into one table. This table is re-imported into GEE as an asset, and the map values from the data to be validated are then added to the table, and it is re-exported, in [GEE script 4](https://code.earthengine.google.com/095b34fe3226cc32553fe4fdf3824a73). 
+
+### Validation
+Once the full table has been assembled, the script 02_change_dataprep.R or 02_dataprep.R are used to clean and recode the data for analysis in R, as well as to process the area tables. 
+
+The analysis R scripts (all script beginning with a *03*) are then used to analyze the prepared data and produce estimates of overall, user's, and producer's accuracy. Estimates of class area are all produced with uncertainties. 
