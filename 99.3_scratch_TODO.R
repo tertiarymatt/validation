3ad3498bc7fd4a3caa304891c9de1d0efe7e71d5
M.S. Patterson <tertiarymatt@gmail.com>
2019-03-08 01:56:53 -0700

    update todo

--- 99.3_scratch_TODO.R
+++ 99.3_scratch_TODO.R
@@ -13,19 +13,19 @@
 #' 2. [x] **Collate CEO outputs:** Bring together all samples, unify with original 
 #' data derived from GEE/MAE to get stratifying map classes and old validation
 #' back into the file. Should be able to join on Lat/Long. 
-#' 3. [] **map_label:**  Data goes back into GEE to sample final maps.
+#' 3. [X] **map_label:**  Data goes back into GEE to sample final maps.
 #' Obviously constrained by existence of final maps.  
-#' 4. [] **Process CEO outputs:** run through data prep scripts to get ref classes.
+#' 4. [X] **Process CEO outputs:** run through data prep scripts to get ref classes.
 #' Will likely require updating the functions, again. Might have to wait on 
 #' this step for the actual maps to be output, to avoid another reclass on the 
 #' data. Potential chokepoint.  
-#' 5. [] **ref_label:**  Pull out of processed CEO data. Can be completed prior
+#' 5. [X] **ref_label:**  Pull out of processed CEO data. Can be completed prior
 #' to having final maps, but is potential chokepoint linked to processing.    
-#' 6. [] **sample_totals:** Derived from a summary of the collatted CEO data.      
-#' 7. [] **rfcodes:**  Easy-peasy lemon squeezy. Reclass possible, though.  
-#' 8. [] **pixel**  = 30.  
-#' 9. [] **totarea_pix:** Easy to get from total areas.  
-#' 10. [] **Analyze:** Use new functions to rebalance sample and calculate 
+#' 6. [X] **sample_totals:** Derived from a summary of the collatted CEO data.      
+#' 7. [X] **rfcodes:**  Easy-peasy lemon squeezy. Reclass possible, though.  
+#' 8. [X] **pixel**  = 30.  
+#' 9. [X] **totarea_pix:** Easy to get from total areas.  
+#' 10. [X] **Analyze:** Use new functions to rebalance sample and calculate 
 #' accuracies. Rejoice, maybe!  
 #' 11. [] **DREAD:** Reclass to improve accuracy and re-run.  
 #' 12. [] **Report:** Outline shortly, then simply fill in. 
